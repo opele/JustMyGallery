ea115d4598ab5c05a2e51d206dfcfe66442cf927
elko <elko.o@web.de>
2021-06-17 21:29:01 +0100

    Update rating.js
    restore rating

--- js/rating.js
+++ js/rating.js
@@ -1,20 +1,20 @@
-function displayRating(imageTarget) {
+function displayRating(imageSrc) {
 
-	var rateEl = $('.galleria-info').find('#rating');
-	if (rateEl.length === 0)
-		rateEl = $('.galleria-info').append(ratingHtml).find('#rating');
+	var rateEl = $(ratingEl);
 	
 	// disable arrowkey navigation to the next input which changes the selection of the next image when the rating input box is focused
 	rateEl.on("keydown keyup keyleft keyright", "input", function(event) { 
 			event.preventDefault();
 	});
 	
-	var item = JSON.parse(localStorage.getItem(imageTarget.getAttribute("src")));
+	var item = JSON.parse(localStorage.getItem(imageSrc));
 	
 	refreshRating(rateEl, item == null ? null : item.rating);
 }
 
 function rateImage(event, rating) {
+	
+	if (event) event.stopPropagation();
 
 	if (!isLocalStorageAccepted()) {
 		alert("Unable to remember new rating without permission to use local browser storage.");
@@ -31,15 +31,6 @@ function refreshRating(rateEl, rating) {
 		$(this).get(0).className = isChecked ? 'fa fa-star checked' : 'fa fa-star unchecked';
 	});
 }
-
-var ratingHtml = 
-	'<div id="rating" class="rate">'
-		+ '<span class = "fa fa-star unchecked" onclick=\'rateImage(event, 1)\'></span>'
-		+ '<span class = "fa fa-star unchecked" onclick=\'rateImage(event, 2)\'></span>'
-		+ '<span class = "fa fa-star unchecked" onclick=\'rateImage(event, 3)\'></span>'
-		+ '<span class = "fa fa-star unchecked" onclick=\'rateImage(event, 4)\'></span>'
-		+ '<span class = "fa fa-star unchecked" onclick=\'rateImage(event, 5)\'></span>'
-	+ '</div>';
  
  
  function processKeyEvtForRating(event) {
