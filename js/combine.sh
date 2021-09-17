2253f76b30fc462f79cf7ede3da6bb5a118aff28
elko <elko.o@web.de>
2021-09-17 21:26:01 +0100

    add combine script for MAC

---
+++ js/combine.sh
@@ -0,0 +1,5 @@
+rm combined.js
+touch combined.js
+rm combined.min.js
+touch combined.min.js
+cat utils.js gallery.js previewImagesLoading.js globalEvents.js sorting.js userDefinedTags.js filtering.js searchAndFilterSidebar.js settingsSidebar.js rating.js imageViewing.js bookmarks.js > combined.js
